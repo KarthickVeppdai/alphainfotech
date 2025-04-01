bd075176e18cfcde7c258c3812cc05f3a24a9936
KarthickVeppdai <karthickjava1997@gmail.com>
2025-04-01 14:32:38 +0530

    first commit

---
+++ alphainfotech/src/main/java/com/Alpha/alphainfotech/AlphainfotechApplication.java
@@ -0,0 +1,13 @@
+package com.Alpha.alphainfotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphainfotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphainfotechApplication.class, args);
+	}
+
+}
---
+++ alphainfotech/src/test/java/com/Alpha/alphainfotech/AlphainfotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alpha.alphainfotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphainfotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
